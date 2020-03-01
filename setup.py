71afe90e67e8309c0dde59bbbd11ead39136cb2c
Adewale Azeez <azeezadewale98@gmail.com>
2020-03-01 10:10:20 +0100

    create the milkish setup file and test

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    zip_safe=False,
+    install_requires=["sphinx>=1.1"],
+    name="milkish",
+    version=1.0,
+    author="Adewale Azeez",
+    author_email="azeezadewale98@gmail.com",
+    description="A milkish looking sphinx theme",
+    long_description=long_description,
+    license="CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+    packages=("milkish",),
+    include_package_data=True,
+    keywords="sphinx extension theme",
+    url="https://github.com/Thecarisma/milkish/",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Common Public License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Documentation',
+    ]
+)
