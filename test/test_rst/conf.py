a748e324270d71428a08b55b4d1733e5153bd407
Adewale Azeez <azeezadewale98@gmail.com>
2020-03-28 03:01:19 +0100

    add them option to hide navigator in index page for fluid theme

--- test/test_rst/conf.py
+++ test/test_rst/conf.py
@@ -94,8 +94,7 @@ html_theme_options = {
     'project_icon': "https://media2.giphy.com/media/2vnId4IaAjIGZd2EWC/giphy.gif",
     'has_left_sidebar': True,
     'has_right_sidebar': True,
-    'show_navigators': True,
-    'show_navigators_in_index': False,
+    'show_navigators': False,
     'left_sidebar_only': [
         "leftpage"
     ],
@@ -120,7 +119,6 @@ html_theme_options = {
     ],
     'footer_menus': [
         {
-            'icon': 'fa-phone',
             'title': 'Contact',
             'menu_items': [
                 {
@@ -134,7 +132,6 @@ html_theme_options = {
             ]
         },
         {
-            'icon': 'fa-book',
             'title': 'Documentation',
             'menu_items': [
                 {
@@ -160,7 +157,6 @@ html_theme_options = {
             ]
         },
         {
-            'icon': 'fa-users',
             'title': 'Community',
             'menu_items': [
                 {
@@ -184,4 +180,4 @@ html_theme_options = {
     ],
 }
 
-html_theme = 'milkish'
+html_theme = 'fluid'
