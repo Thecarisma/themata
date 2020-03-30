ad9f35b7acac96d58714d8e1a1465b82a04c43bd
Adewale Azeez <azeezadewale98@gmail.com>
2020-03-30 20:29:40 +0100

    implements the garri theme

---
+++ themata/garri/__init__.py
@@ -0,0 +1,22 @@
+import os
+
+__version_trait = (1, 0, 0)
+__version = ".".join(map(str, __version_trait))
+
+def get_path():
+    """
+    Return the path to the garri theme
+    """
+    return os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+
+def update_context(app, pagename, templatename, context, doctree):
+    context["garri_version"] = __version
+
+
+def setup(app):
+    if hasattr(app, "add_html_theme"):
+        theme_path = os.path.abspath(os.path.dirname(__file__))
+        app.add_html_theme("garri", theme_path)
+    app.connect("html-page-context", update_context)
+    return {"version": __version}
