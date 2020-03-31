ae95abf246e03db1a376e3787df3450246a34f1c
Adewale Azeez <azeezadewale98@gmail.com>
2020-03-31 04:07:06 +0100

    creates the clear theme documenatation

---
+++ docs/gendoc.bat
@@ -0,0 +1,28 @@
+@echo off
+
+cd ..\
+SET OUTPUT_FOLDER=%CD%\dist\
+cd docs/
+SET THEMES=clear
+if exist "%OUTPUT_FOLDER%" (
+    @RD /S /Q "%OUTPUT_FOLDER%"
+)
+md %OUTPUT_FOLDER%
+
+cmd.exe /c make.bat html
+move "build\html\*" "%OUTPUT_FOLDER%\"
+for /d %%a in ("build\html\*") do move "%%~fa" "%OUTPUT_FOLDER%\"
+
+(for %%a in (%THEMES%) do (
+    echo.
+    echo ==================================
+    echo generating doc for %%a
+    echo ==================================
+    echo.
+    cd %%a
+    cmd.exe /c make html 
+    mkdir %OUTPUT_FOLDER%\%%a\
+    move "build\html\*" "%OUTPUT_FOLDER%\%%a\"
+    for /d %%b in ("build\html\*") do move "%%~fb" "%OUTPUT_FOLDER%\%%a\"
+    cd ..\
+))
