46f4f052939c2bb709f0d7e3d3bf3299a4edc34d
Adewale Azeez <azeezadewale98@gmail.com>
2020-04-02 01:58:32 +0100

    remove 'show source' from the genindex page

--- test/test_rst/conf.py
+++ test/test_rst/conf.py
@@ -8,90 +8,12 @@ author = 'Adewale Azeez'
 html_theme_path = [themata.get_html_theme_path()]
 
 html_theme_options = {
-    'project_icon': "https://media2.giphy.com/media/2vnId4IaAjIGZd2EWC/giphy.gif",
-    'has_left_sidebar': True,
-    'show_navigators': True,
-    'show_navigators_in_index': False,
-    'left_sidebar_only': [
-        "leftpage"
-    ],
+    'sidebar_position': 'right',
+    'has_sidebar': True,
     'no_sidebar': [
         "singlepage"
     ],
-    'navbar_links': [
-        ("Docs", "./docs"),
-        ("Download", "https://pypi.python.org/pypi/milkish"),
-        ("Examples", "./examples"),
-        ("FAQ", "./faq")
-    ],
-    'social_icons': [
-        ("fab fa-twitter", "https://twitter.com/iamthecarisma"),
-        ("fab fa-twitch", "https://www.twitch.tv/amsiraceht"),
-        ("fab fa-github", "https://github.com/Thecarisma"),
-        ("fab fa-linkedin", "https://www.linkedin.com/in/azeez-adewale/"),
-        ("fab fa-stackoverflow", "https://stackoverflow.com/users/6626422/thecarisma")
-    ],
-    'footer_menus': [
-        {
-            'title': 'Contact',
-            'menu_items': [
-                {
-                    'link': "#",
-                    'title': 'azeezadewale98@gmail.com'
-                },
-                {
-                    'link': "",
-                    'title': '12345678998'
-                }
-            ]
-        },
-        {
-            'title': 'Documentation',
-            'menu_items': [
-                {
-                    'link': "#",
-                    'title': 'Title One'
-                },
-                {
-                    'link': "#",
-                    'title': 'Two'
-                },
-                {
-                    'link': "#",
-                    'title': 'Three'
-                },
-                {
-                    'link': "#",
-                    'title': 'Title Four'
-                },
-                {
-                    'link': "#",
-                    'title': 'Item Five'
-                },
-            ]
-        },
-        {
-            'title': 'Community',
-            'menu_items': [
-                {
-                    'link': "#",
-                    'title': 'Stackoverflow'
-                },
-                {
-                    'link': "",
-                    'title': 'Slack'
-                },
-                {
-                    'link': "",
-                    'title': 'Forum'
-                },
-                {
-                    'link': "",
-                    'title': 'IRC'
-                }
-            ]
-        }
-    ],
+    'toc_title': 'Main Links'
 }
 
-html_theme = 'hackish'
+html_theme = 'garri'
