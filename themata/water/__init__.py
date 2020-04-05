b6945abb13eaf4c5afb6e2f97d1ea4a0a15b3815
Adewale Azeez <azeezadewale98@gmail.com>
2020-04-05 22:40:06 +0100

    completes the water theme implementation

---
+++ themata/water/__init__.py
@@ -0,0 +1,22 @@
+import os
+
+__version_trait = (1, 0, 0)
+__version = ".".join(map(str, __version_trait))
+
+def get_path():
+    """
+    Return the path to the water theme
+    """
+    return os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+
+def update_context(app, pagename, templatename, context, doctree):
+    context["water_version"] = __version
+
+
+def setup(app):
+    if hasattr(app, "add_html_theme"):
+        theme_path = os.path.abspath(os.path.dirname(__file__))
+        app.add_html_theme("water", theme_path)
+    app.connect("html-page-context", update_context)
+    return {"version": __version}
