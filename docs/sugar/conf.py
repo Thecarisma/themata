662f9cf3397412076b2d3a17892c1d6c8c9c4fb7
Adewale Azeez <azeezadewale98@gmail.com>
2020-04-06 08:44:06 +0100

    completes the static test for sugar theme and documentation

---
+++ docs/sugar/conf.py
@@ -0,0 +1,69 @@
+import os
+import themata
+
+project = 'Sugar'
+copyright = '2020, Adewale Azeez, Creative Commons Zero v1.0 Universal License'
+author = 'Adewale Azeez'
+
+html_theme_path = [themata.get_html_theme_path()]
+html_theme = 'sugar'
+html_favicon = '../images/themata.png'
+
+html_theme_options = {
+    'index_is_single': False,
+    'show_navigators_in_index': False,
+    'collapsible_sidebar': True,
+    'collapsible_sidebar_display': 'block',
+    'navbar_links': [
+        ('Twitter', 'https://twitter.com/iamthecarisma'),
+        ('Github', 'https://github.com/Thecarisma/themata/')
+    ],
+    'navbar_sec_links': [
+        ('Twitter', 'https://twitter.com/iamthecarisma'),
+        ('Github', 'https://github.com/Thecarisma/themata/')
+    ],
+    'has_left_sidebar': True,
+    'has_right_sidebar': True,
+    'show_navigators': True,
+    'footer_menus': [
+        {
+            'title': 'Contact',
+            'menu_items': [
+                {
+                    'link': 'https://thecarisma.github.io/',
+                    'title': 'https://thecarisma.github.io/'
+                },
+                {
+                    'link': 'tel:911',
+                    'title': '12345678998'
+                }
+            ]
+        },
+        {
+            'title': 'Custom Pages',
+            'menu_items': [
+                {
+                    'link': 'leftpage.html',
+                    'title': 'Left Page'
+                },
+                {
+                    'link': 'singletpage.html',
+                    'title': 'Single Page'
+                }
+            ]
+        }
+    ],
+    'social_icons': [
+        ('fab fa-twitter', 'https://twitter.com/iamthecarisma'),
+        ('fab fa-github', 'https://github.com/Thecarisma/themata/')
+    ],
+    'left_sidebar_only': [
+        'otherpages/leftpage'
+    ],
+    'no_sidebar': [
+        'otherpages/singlepage'
+    ],
+    'right_sidebar_only': [
+        'otherpages/rightpage'
+    ]
+}
